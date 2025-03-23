c935b18577aa73500c3c8b69757ce5926953c51a
Noah Holloway <noahholloway23@gmail.com>
2025-03-23 17:37:55 -0600

    code

---
+++ DinerMenu/Diner Menu.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class DinerMenuForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(657, 359)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(131, 79)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(446, 359)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(131, 79)
+        Me.FishButton.TabIndex = 1
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(264, 359)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(131, 79)
+        Me.SaladButton.TabIndex = 2
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(73, 359)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(131, 79)
+        Me.SoupButton.TabIndex = 3
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(234, 18)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(334, 32)
+        Me.DinerNameLabel.TabIndex = 4
+        Me.DinerNameLabel.Text = "Resturaunte de Comida"
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.AutoSize = True
+        Me.DisplaySpecialLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(68, 88)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(395, 25)
+        Me.DisplaySpecialLabel.TabIndex = 5
+        Me.DisplaySpecialLabel.Text = "Select a menu option to view todays special!"
+        Me.DisplaySpecialLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Name = "DinerMenuForm"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents DisplaySpecialLabel As Label
+End Class
---
+++ DinerMenu/Diner Menu.vb
@@ -0,0 +1,57 @@
+﻿'Noah Holloway
+'RCET 2265
+'Spring 2025
+'Diner Menu
+'
+
+Option Strict On
+Option Explicit On
+Option Compare Text
+
+Public Class DinerMenuForm
+    Private Sub DinerNameLabel_Click(sender As Object, e As EventArgs) Handles DinerNameLabel.Click
+        Me.Text = "Restaraunte de Comida Special Menu"
+        DisplaySpecialLabel.Text = "Select a menu option to view todays special!"
+    End Sub
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        DisplaySpecialLabel.Text =
+            "La sopa del dia" & vbCrLf &
+          "~ Cozy Chicken & Gnocchi Soup ~" & vbCrLf & vbCrLf &
+            "Warm up with a bowl of pure comfort! Our Chicken & Gnocchi Soup" & vbCrLf &
+            "is packed with tender roasted chicken, pillowy potato gnocchi," & vbCrLf &
+            "and a creamy, herb-infused broth that'll make you feel like" & vbCrLf &
+            "you're wrapped in a grandma-approved hug. A touch of nutmeg," & vbCrLf &
+            "a sprinkle of parmesan, and a handful of baby spinach" & vbCrLf &
+            "complete this heartwarming dish. Grab a spoon and dive in!"
+    End Sub
+
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        DisplaySpecialLabel.Text =
+           "La enselada del dia" & vbCrLf &
+            "~ Mandarin Walnut Bliss Salad ~" & vbCrLf & vbCrLf &
+            "Sweet, tangy, crunchy, and fresh—this salad has it all!" & vbCrLf &
+            "Juicy mandarin oranges, toasted walnuts, and crumbled feta" & vbCrLf &
+            "rest on a bed of crisp romaine and baby spinach. Tossed in a" & vbCrLf &
+            "zingy honey-lime vinaigrette, this salad is the perfect balance" & vbCrLf &
+            "of flavors. Add some grilled chicken for extra heartiness," & vbCrLf &
+            "or just enjoy it as is—because it's already pretty perfect!"
+    End Sub
+
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        DisplaySpecialLabel.Text =
+             "El Pescado del dia" & vbCrLf &
+            "~ Honey Chipotle Smoked Salmon ~" & vbCrLf & vbCrLf &
+            "Sweet, smoky, and just the right amount of heat—this dish" & vbCrLf &
+            "is a flavor explosion! Our fresh Atlantic salmon is smoked" & vbCrLf &
+            "to perfection, then glazed with a house-made honey-chipotle sauce." & vbCrLf &
+            "Served with a side of garlic butter asparagus and roasted red potatoes," & vbCrLf &
+            "this meal will make you wonder why you ever ate anything else." & vbCrLf &
+            "Spice up your life—order this salmon today!"
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+End Class
